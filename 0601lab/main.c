49a66d88521805540e9eb26e860fd52e17534aaa
天野由梨 <x19005xx@aitech.ac.jp>
2021-06-01 11:49:49 +0900

    first commit

---
+++ 0601lab/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  0601lab
+//
+//  Created by x19005xx on 2021/06/01.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("AAAAAA\n");
+    return 0;
+}
